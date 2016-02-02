c53c9563e0c13fab89d1cfb447e918afc90aecd2
Stéphane Bérubé <sirber@hotmail.com>
2016-02-02 09:47:21 -0500

    removed: monolog (didn't work) fixed: structure

--- private/app.php
+++ private/app.php
@@ -2,41 +2,37 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-# Vendor
+## Vendor
 require '../vendor/autoload.php';
 
-# App
+## App
 $settings = require "settings.php";
 $app = new \Slim\App($settings);
 session_start();
 
-# Dependencies
+## Dependencies
 $container = $app->getContainer();
+# view renderer
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/templates', [
+        'cache' => false
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+    return $view;
 };
 
-// monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
-    return $logger;
-};
+## Middleware
 
-# Middleware
-
-
-# Routes
+## Routes
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
-
+	
     return $response;
 });
 $app->run();
--- private/settings.php
+++ private/settings.php
@@ -2,16 +2,5 @@
 return [
     'settings' => [
         'displayErrorDetails' => true,
-
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/view/',
-        ],
-
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => __DIR__ . '/../logs/app.log',
-        ],
     ],
 ];
