1817a4a9ca2da43446824fe4d82d3b3d0486a333
Stéphane Bérubé <sirber@hotmail.com>
2016-02-02 13:10:32 -0500

    added: index to sql structure

--- private/doc/sql/app_stylorouge.sql
+++ private/doc/sql/app_stylorouge.sql
@@ -5,20 +5,17 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP DATABASE IF EXISTS `app_stylorouge`;
-CREATE DATABASE `app_stylorouge` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `app_stylorouge`;
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=Aria AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci PAGE_CHECKSUM=1;
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
 (1,	'sirber',	'$2y$10$aHBlzsKeyjt07A/HEErCxuUclDisCOLyuBCc21bzwcgvvp9BQxqvG',	'sirber@hotmail.com');
 
--- 2016-02-02 17:59:23
+-- 2016-02-02 18:09:51
