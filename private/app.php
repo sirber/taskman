3cdffa896f46f36c72156744813b1a0cd028a2ca
Stéphane Bérubé <sirber@hotmail.com>
2016-02-04 10:13:11 -0500

    added: refresh session user info on every page added: user info added to template (default vars)

--- private/app.php
+++ private/app.php
@@ -39,6 +39,14 @@ $app->add(function($request, $response, $next) {
 		// redirects to login
 		return $response->withRedirect($this->router->pathFor('login'), 303);
 	}
+	else {
+		// refresh user info
+		$datas = $this->db->select("user", "*", ["id" => $_SESSION["user_id"]]);
+		$_SESSION["user"] = $datas[0];
+	}
+	
+	# Add user info to template
+	$this->view->offsetSet('user', $_SESSION["user"]);
 	
 	# Verify ACL
 	
@@ -76,10 +84,8 @@ $app->group('/user', function () {
 			if (password_verify($args["password"], $user["password"])) {
 				// wohoo!
 				$_SESSION["user_id"] = $user["id"]; // basic
-                $_SESSION["user"] = $user; // full
                 
-				session_regenerate_id();	
-				
+				session_regenerate_id();					
 				return $response->withRedirect($this->router->pathFor('task', []), 303);
 			}		
 		}
@@ -97,7 +103,7 @@ $app->group('/user', function () {
 $app->group('/task', function () {
     $this->get('', function ($request, $response, $args) {
         $datas = $this->db->select('task', '*', []);
-        return $this->view->render($response, 'task.html', ['user' => $_SESSION['user'], 'datas' => $datas]);
+        return $this->view->render($response, 'task.html', ['datas' => $datas]);
     })->setName('task');        
 });
 
